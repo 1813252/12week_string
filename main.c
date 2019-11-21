72c1b366b753f578d417198d35948c3d29928c93
1813252 <55134827+1813252@users.noreply.github.com>
2019-11-21 15:21:25 +0900

    fprintf,fscanf

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
+	FILE *fp;
+	int i;
+	char word[100];
 	
+	fp=fopen("sample.txt","r");
 	
-	int i=0;
-	char str[4];
-	str[0]='a';
-	str[1]='b';
-	str[2]='c';
-	str[3]='\0';
+	for(i=0;i<3;i++){
+		
 	
-	printf("%s\n",str);
-	
-	while (str[i]!=0)
-	{
-		printf("%c",str[i]);
-		i++;
+		fscanf(fp, "%s",word); 
+		printf("%s\n",word);
+
 	}
 	
+	fclose(fp);
 	
 	
 	return 0;
